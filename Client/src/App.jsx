41fa6104eaa476899b1d050b80caadb88bd4131f
Bibi-owoeye <owoeyebibi1@gmail.com>
2024-07-16 09:03:42 -0700

    update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Nav from './components/Nav'
 import { Route , Routes } from 'react-router'
 import Ticket1 from './components/Ticket1'
-import Register from './components/Register'
+// import Register from './components/Register'
 // import Card from './components/Card'
 import ChangeEmail from './components/ChangeEmail';
 import Password from './components/Password';
@@ -17,7 +17,7 @@ const App = () => {
         <Route path='/' element= {<Nav/>}/>
         <Route path='/ticket' element= {<Ticket1/>}/>
         <Route path='/card' element= {<CardDetails/>}/>
-        <Route path='/register' element= {<Register/>}/>
+        {/* <Route path='/register' element= {<Register/>}/> */}
         <Route path="/accountInfo" element={<AcctSettingInfo />}/>
         <Route path='/changeEmail' element={<ChangeEmail />}/>
         <Route path='/Password' element={<Password />}/>
